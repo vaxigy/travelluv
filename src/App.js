996403121ef0cd527fa721cf02958d73cbef5215
vaxigy <vip.vaxyly@gmail.com>
2025-09-10 23:08:55 +0300

    Reset scroll position on page change

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './styles/main.scss';
 import Header from './components/Header.jsx';
 import Main from './components/Main.jsx';
 import Footer from './components/Footer.jsx';
+import ScrollResetter from './components/ScrollResetter.jsx';
 
 function App() {
   return (
     <div className='App'>
+      <ScrollResetter />
       <Header />
       <Main />
       <Footer />
